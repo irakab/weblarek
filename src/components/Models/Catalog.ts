112d720943901a126f2c873d60201304d1b8d46b
irasin <soosleek@me.com>
2025-12-18 14:35:53 +0200

    step1-2, interfaces and class Catalog

---
+++ src/components/Models/Catalog.ts
@@ -0,0 +1,31 @@
+import { IProduct } from "../../types/index";
+
+
+class Catalog {
+    products : IProduct[] = [];
+    selectedProduct : IProduct | null = null;
+    
+    constructor () {}
+
+    setProducts (products : IProduct[]) : void {  //сохранить массив из продуктов
+        this.products = products;
+    }
+
+    getProducts () : IProduct[] {  //получить массив продуктов
+        return this.products
+    }
+    setSelected(product : IProduct) : void { //сохранение выбранного
+        this.selectedProduct = product;
+    }
+    getSelected() : IProduct | null { //получение выбранного
+            return this.selectedProduct
+    }
+    resetSelected() : void {
+        this.selectedProduct = null; //сбрасываем выбор
+    }
+
+    getProductById(id: string) : IProduct | undefined { //получить продукт но id
+        return this.products.find(product => product.id === id)
+   
+ }
+}
--- src/types/index.ts
+++ src/types/index.ts
@@ -4,3 +4,23 @@ export interface IApi {
     get<T extends object>(uri: string): Promise<T>;
     post<T extends object>(uri: string, data: object, method?: ApiPostMethods): Promise<T>;
 }
+
+
+export interface IProduct {
+    id: string;
+    title: string;
+    image: string;
+    categoty: string;
+    price: number | null;
+    description: string;
+}
+
+export interface IBuyer {
+    payment: TPayment;
+    email: string;
+    phone: string;
+    address: string;
+}
+
+export type TPayment = "online" | "cash" | null;
+
